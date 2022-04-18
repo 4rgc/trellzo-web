f23ad393fbf1b103d5df84949ad515f85e5d018f
4rgc <bohdan.andrii@gmail.com>
2022-04-18 16:04:31 -0400

    test: add tests and stories for a Button

---
+++ src/components/Button/__stories__/Button.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from '../Button';
+
+export default {
+	title: 'Components/Button',
+	component: Button,
+	argTypes: { onClick: { action: 'clicked' } },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = ({ children, ...args }) => (
+	<Button {...args}>{children}</Button>
+);
+
+export const Default = Template.bind({});
+
+export const Square = Template.bind({});
+Square.args = { size: 'square', children: 'OK' };
+
+export const Small = Template.bind({});
+Small.args = { size: 'small', children: 'Small' };
+
+export const Medium = Template.bind({});
+Medium.args = { size: 'medium', children: 'Medium Btn' };
+
+export const Large = Template.bind({});
+Large.args = { size: 'large', children: 'Large Button !!!' };
+
+export const Primary = Template.bind({});
+Primary.args = { kind: 'primary', children: 'Primary' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { kind: 'secondary', children: 'Secondary' };
+
+export const Disabled = Template.bind({});
+Disabled.args = { disabled: true, children: 'disabled' };
+
+export const CustomClass = Template.bind({});
+CustomClass.args = {
+	disabled: false,
+	children: 'fake disabled',
+	className: 'btn-dis',
+};
---
+++ src/components/Button/__test__/Button.test.tsx
@@ -0,0 +1,33 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { Button } from '../Button';
+
+describe('Button', () => {
+	it('calls onClick when button is clicked', () => {
+		const handler = jest.fn();
+
+		render(<Button onClick={handler}>Button</Button>);
+
+		const button = screen.getByText(/button/i);
+
+		userEvent.click(button);
+
+		expect(handler).toHaveBeenCalledTimes(1);
+	});
+
+	it("doesn't call onClick if button is disabled", () => {
+		const handler = jest.fn();
+
+		render(
+			<Button onClick={handler} disabled={true}>
+				Button
+			</Button>
+		);
+
+		const button = screen.getByText(/button/i);
+
+		userEvent.click(button);
+
+		expect(handler).not.toHaveBeenCalled();
+	});
+});
