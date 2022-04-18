38dbf8236ab63d9fd4001ed87b12eb3014a683ed
4rgc <bohdan.andrii@gmail.com>
2022-04-18 16:14:36 -0400

    feat: use fetcher in useTrellzoAPI hook instead of useSWR

--- src/hooks/useTrellzoAPI/__tests__/useTrellzoAPI.test.tsx
+++ src/hooks/useTrellzoAPI/__tests__/useTrellzoAPI.test.tsx
@@ -1,70 +1,187 @@
 import useTrellzoAPI from '../useTrellzoAPI';
-import useSWR from 'swr';
 import APIRequestParams from '../../../util/APIParams';
-const BASE_URL = 'localhost:3000';
+import useFetcher from '../../useFetcher/useFetcher';
+import { Dispatch } from 'react';
 
-jest.mock('swr');
+const BASE_URL = 'http://localhost:3000';
+
+jest.mock('../../useFetcher/useFetcher');
+jest.mock('../../../util/fetcher.ts');
 
 describe('useTrellzoAPI', () => {
-	const mockSwr = (res: any = { data: { status: 'ok' } }) => {
-		(useSWR as jest.Mock).mockImplementationOnce(() => res);
+	const mockUseFetcher = (
+		data: any = { success: true },
+		error: Error | undefined = undefined,
+		customHandlerParams: [
+			Response | undefined,
+			Dispatch<any>,
+			Dispatch<Error | undefined>
+		] = [undefined, () => {}, () => {}]
+	) => {
+		(useFetcher as jest.Mock).mockImplementationOnce(
+			(initUrl, initParams, customHandler?) => {
+				return [
+					data,
+					error,
+					() => {
+						if (customHandler)
+							customHandler(...customHandlerParams);
+					},
+					() => {},
+				];
+			}
+		);
 	};
 
-	it('passes empty string into useSWR when route is empty', () => {
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('passes empty string into fetcher when route is empty', () => {
 		const params = new APIRequestParams();
 
-		mockSwr();
+		mockUseFetcher();
 
 		useTrellzoAPI(params);
 
-		expect(useSWR).toHaveBeenCalledWith(
-			['', params],
+		expect(useFetcher).toHaveBeenCalledWith(
+			'',
 			expect.anything(),
-			expect.anything()
+			expect.any(Function)
 		);
 	});
 
-	it('passes correct url into useSWR when route is set', () => {
+	it('passes correct url into useFetcher when route is set', () => {
 		const params = new APIRequestParams();
 		params.setRoute('/');
 
-		mockSwr();
+		mockUseFetcher();
 
 		useTrellzoAPI(params);
 
-		expect(useSWR).toHaveBeenCalledWith(
-			[`${BASE_URL}/`, params],
+		expect(useFetcher).toHaveBeenCalledWith(
+			`${BASE_URL}/`,
 			expect.anything(),
-			expect.anything()
+			expect.any(Function)
 		);
 	});
 
-	it('passes url params into useSWR', () => {
+	it('passes url params into useFetcher', () => {
 		const params = new APIRequestParams();
 		params.setRoute('/');
 		params.setUrlParam('a', 'b');
 
-		mockSwr();
+		mockUseFetcher();
 
 		useTrellzoAPI(params);
 
-		expect(useSWR).toHaveBeenCalledWith(
-			[`${BASE_URL}/?a=b`, params],
+		expect(useFetcher).toHaveBeenCalledWith(
 			expect.anything(),
+			params,
 			expect.anything()
 		);
 	});
 
-	it('returns data and error from useSWR', () => {
+	it('returns data and error from useFetcher', () => {
 		const params = new APIRequestParams();
 
-		mockSwr({
-			data: 'data',
-			error: 'error',
-		});
+		const error = new Error('error');
+		mockUseFetcher({ data: 'data' }, error);
 
 		const result = useTrellzoAPI(params);
 
-		expect(result).toStrictEqual(['data', 'error']);
+		expect(result).toStrictEqual([
+			{ data: 'data' },
+			error,
+			expect.any(Function),
+			expect.any(Function),
+		]);
+	});
+
+	it('sets an empty response error', async () => {
+		const params = new APIRequestParams();
+		const setDataMock = jest.fn();
+		const setErrorMock = jest.fn();
+
+		mockUseFetcher(undefined, undefined, [
+			undefined,
+			setDataMock,
+			setErrorMock,
+		]);
+
+		const [data, error, login, changeParams] = useTrellzoAPI(params);
+
+		await login();
+
+		expect(setDataMock).not.toHaveBeenCalled();
+		expect(setErrorMock).toHaveBeenCalledWith(expect.any(Error));
+	});
+
+	it('sets data on 200, 201', async () => {
+		const params = new APIRequestParams();
+		const setDataMock = jest.fn();
+		const setErrorMock = jest.fn();
+
+		const res = new Response(undefined, { status: 200 });
+		res.json = () => Promise.resolve('data');
+
+		mockUseFetcher(undefined, undefined, [res, setDataMock, setErrorMock]);
+
+		const [data, error, login, changeParams] = useTrellzoAPI(params);
+
+		await login();
+
+		expect(setErrorMock).not.toHaveBeenCalled();
+		expect(setDataMock).toBeCalledWith('data');
+	});
+
+	it('sets error on 400-600', async () => {
+		const params = new APIRequestParams();
+		const setDataMock = jest.fn();
+		const setErrorMock = jest.fn();
+
+		for (let status of [400, 403, 500, 510]) {
+			const res = new Response(undefined, { status });
+			res.json = () => Promise.resolve({ message: 'error' });
+
+			mockUseFetcher(undefined, undefined, [
+				res,
+				setDataMock,
+				setErrorMock,
+			]);
+
+			//eslint-disable-next-line react-hooks/rules-of-hooks
+			const [data, error, login, changeParams] = useTrellzoAPI(params);
+
+			await login();
+
+			expect(setDataMock).not.toHaveBeenCalled();
+			expect(setErrorMock).toHaveBeenCalledWith(expect.any(Error));
+		}
+	});
+
+	it('sets error on all other status codes', async () => {
+		const params = new APIRequestParams();
+		const setDataMock = jest.fn();
+		const setErrorMock = jest.fn();
+
+		for (let status of [101, 300, 206, 210]) {
+			const res = new Response(undefined, { status });
+			res.json = () => Promise.resolve({ message: 'error' });
+
+			mockUseFetcher(undefined, undefined, [
+				res,
+				setDataMock,
+				setErrorMock,
+			]);
+
+			//eslint-disable-next-line react-hooks/rules-of-hooks
+			const [data, error, login, changeParams] = useTrellzoAPI(params);
+
+			await login();
+
+			expect(setDataMock).not.toHaveBeenCalled();
+			expect(setErrorMock).toHaveBeenCalledWith(expect.any(Error));
+		}
 	});
 });
--- src/hooks/useTrellzoAPI/useTrellzoAPI.tsx
+++ src/hooks/useTrellzoAPI/useTrellzoAPI.tsx
@@ -1,24 +1,56 @@
-import useSWR from 'swr';
-import { PublicConfiguration } from 'swr/dist/types';
+import { Dispatch } from 'react';
 import APIRequestParams from '../../util/APIParams';
-import fetcher from '../../util/fetcher';
+import useFetcher from '../useFetcher/useFetcher';
 
-const useTrellzoAPI = (params: APIRequestParams) => {
-	// TODO: use env variable
-	const baseUrl = 'localhost:3000';
-
-	const apiUrl = params.getRoute()
+const buildUrl = (baseUrl: string, params?: APIRequestParams) =>
+	params && params.getRoute()
 		? `${baseUrl}${params.getRoute()}${params.getUrlParamsString()}`
 		: '';
 
-	const config: Partial<PublicConfiguration> = {};
-	config.refreshInterval = 0;
-	config.revalidateOnMount = false;
-	config.shouldRetryOnError = false;
+const useTrellzoAPI = (initialParams: APIRequestParams) => {
+	// TODO: use env variable
+	const baseUrl = 'http://localhost:3000';
 
-	const { data, error } = useSWR([apiUrl, params], fetcher, config);
+	const initialUrl = buildUrl(baseUrl, initialParams);
 
-	return [data, error];
+	const customHandler = async (
+		res: Response | undefined,
+		setData: Dispatch<any>,
+		setError: Dispatch<Error | undefined>
+	) => {
+		if (!res) {
+			setError(new Error('API Error: Empty response'));
+			return;
+		}
+
+		if ([200, 201].includes(res.status)) {
+			const data = await res.json();
+			setData(data);
+		} else if (res.status >= 400 && res.status < 600) {
+			const body = await res.json();
+			setError(new Error(`API Error: ${res.status} ${body.message}`));
+		} else {
+			setError(
+				new Error(
+					`API Error: ${res.status} – unimplemented or invalid response status`
+				)
+			);
+		}
+	};
+
+	const [data, error, reload, changeParams] = useFetcher(
+		initialUrl,
+		initialParams,
+		customHandler
+	);
+
+	const changeParamsWrapper = (params?: APIRequestParams) =>
+		changeParams({
+			url: buildUrl(baseUrl, params),
+			params,
+		});
+
+	return [data, error, reload, changeParamsWrapper];
 };
 
 export default useTrellzoAPI;
