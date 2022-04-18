544228f58ff9dd7f776cb08121faa22ef9770256
4rgc <bohdan.andrii@gmail.com>
2022-04-18 15:44:29 -0400

    test: configure storybook

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { setGlobalConfig } from '@storybook/testing-react';
+// Storybook's preview file location
+import * as globalStorybookConfig from '../.storybook/preview';
+import React from 'react';
+React.useLayoutEffect = React.useEffect;
+
+setGlobalConfig(globalStorybookConfig);
