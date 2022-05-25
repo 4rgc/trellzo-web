3b7950947292191ded9a62af394885c66e3b3ba6
4rgc <bohdan.andrii@gmail.com>
2022-05-25 18:06:58 +0200

    feat: add react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
+import Boards from './pages/Boards';
 import Login from './pages/Login';
 
 const App: React.FC = () => {
 	return (
+		<Router>
 		<div className="App">
-			<Login />
+				<Routes>
+					<Route index element={<Login />} />
+					<Route element={<Boards />} path="boards" />
+				</Routes>
 		</div>
+		</Router>
 	);
 };
 
