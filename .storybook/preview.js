d6db0138b3a7b3a806d20c832e0b84bab8dea78f
4rgc <bohdan.andrii@gmail.com>
2022-05-28 22:34:54 +0200

    fix: add global styles to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
+import '../src/global.scss';
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
