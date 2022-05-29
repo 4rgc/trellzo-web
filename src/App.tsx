6d6bd1f4fc99823c82f0b23f5e757535cbded890
4rgc <bohdan.andrii@gmail.com>
2022-05-29 19:38:04 +0200

    feat: add a nav element

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import './App.scss';
+import Logo from './components/Logo';
+
 import Boards from './pages/Boards';
 import Login from './pages/Login';
 
 const App: React.FC = () => {
+	const location = useLocation();
+
 	return (
-		<Router>
-			<div className="App">
-				<Routes>
-					<Route index element={<Login />} />
-					<Route element={<Boards />} path="boards" />
-				</Routes>
-			</div>
-		</Router>
+		<div className="App">
+			{location.pathname !== '/login' && (
+				<nav>
+					<Logo />
+				</nav>
+			)}
+			<Routes>
+				<Route path="/" element={<Navigate to="/login" />} />
+				<Route element={<Login />} path="login" />
+				<Route element={<Boards />} path="boards" />
+			</Routes>
+		</div>
 	);
 };
 
