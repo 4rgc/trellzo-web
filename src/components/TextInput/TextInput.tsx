7d643edac34b0fe73226bd2ca026018940fe7a87
Andrii Bohdan <bohdan.andrii@gmail.com>
2022-07-21 23:31:34 -0400

    feat: add the TextInput component (merge PR #11)
    * feat: add the TextInput component
    
    * fix: prop types and onChange property
    
    * feat: set the default node version
    
    * fix: regenerate package-lock after locking node version
    
    * security: remove vulnerable deps
    
    * test: add tests for TextInput
    
    * fix: custom className overriding font size

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,56 @@
+import { ComponentProps } from 'react';
+import './TextInput.scss';
+
+type InputChangeHandler = (text: string) => void;
+
+interface ISingleLineInputProps {
+	variant?: 'single';
+	onChange?: InputChangeHandler;
+	fontSize?: 'md' | 'lg';
+	value?: string;
+}
+
+interface IMultilineInputProps {
+	variant?: 'multi';
+	onChange?: InputChangeHandler;
+	fontSize?: 'md' | 'lg';
+	value?: string;
+	rows?: number;
+	cols?: number;
+}
+
+export type TextInputProps = (ISingleLineInputProps | IMultilineInputProps) &
+	Omit<ComponentProps<'input'> & ComponentProps<'textarea'>, 'onChange'>;
+
+const TextInput: React.FC<TextInputProps> = (props) => {
+	const {
+		variant = 'single',
+		onChange,
+		value,
+		fontSize = 'md',
+		className: customClassName,
+		...otherProps
+	} = props;
+
+	const className = `tinput-${fontSize}${customClassName ? ` ${customClassName}`: ''}`;
+
+	return variant === 'multi' ? (
+		<textarea
+			className={className}
+			onChange={onChange && ((e) => onChange(e.target.value))}
+			value={value}
+			{...otherProps}
+		>
+			{value}
+		</textarea>
+	) : (
+		<input
+			className={className}
+			value={value}
+			onChange={onChange && ((e) => onChange(e.target.value))}
+			{...otherProps}
+		/>
+	);
+};
+
+export default TextInput;
---
+++ src/components/TextInput/__test__/TextInput.test.tsx
@@ -0,0 +1,33 @@
+import TextInput from '../TextInput';
+import { act, render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+
+describe('TextInput', () => {
+	it('calls onChange when input changes', async () => {
+		const handler = jest.fn();
+
+		render(<TextInput value={''} onChange={handler} />);
+
+		const input = await screen.findByRole('textbox');
+
+		await user.click(input);
+		await user.type(input, 'h');
+
+		expect(handler).toHaveBeenCalledTimes(1);
+		expect(handler).toHaveBeenCalledWith('h');
+	});
+
+	it('calls onChange when multiline input changes', async () => {
+		const handler = jest.fn();
+
+		render(<TextInput value={''} onChange={handler} variant="multi" />);
+
+		const input = await screen.findByRole('textbox');
+
+		await user.click(input);
+		await user.type(input, 'i');
+
+		expect(handler).toHaveBeenCalledTimes(1);
+		expect(handler).toHaveBeenCalledWith('i');
+	});
+});
