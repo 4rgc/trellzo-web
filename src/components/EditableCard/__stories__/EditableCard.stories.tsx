c0eeb1592bf796ce460f06c4b49d538dc9a5ec4a
4rgc <bohdan.andrii@gmail.com>
2022-08-04 07:18:48 -0400

    feat: add storybook boilerplate to pass ci

---
+++ src/components/EditableCard/__stories__/EditableCard.stories.tsx
@@ -0,0 +1,6 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import EditableCard from '..';
+
+export default {
+	title: 'Components/EditableCard',
+} as ComponentMeta<typeof EditableCard>;
