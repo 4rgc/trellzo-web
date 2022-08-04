f29d205894437a51e1618347592ca6b0a9a38a68
4rgc <bohdan.andrii@gmail.com>
2022-08-04 08:35:52 -0400

    feat: add basic skeleton for EditableCard

--- src/components/EditableCard/EditableCard.tsx
+++ src/components/EditableCard/EditableCard.tsx
@@ -1,3 +1,67 @@
 import Card from '../Card';
+import TextInput from '../TextInput';
+import './EditableCard.scss';
 
-export default Card;
+interface IEditableCardProps {
+	bodyPlaceholder?: string;
+	bodyValue?: string;
+	onTitleChange?: (text: string) => void;
+	onBodyChange?: (text: string) => void;
+}
+
+type EditableCardPropsWithoutTitle = IEditableCardProps & {
+	hasTitleField?: false;
+};
+type EditableCardPropsWithTitle = IEditableCardProps & {
+	hasTitleField?: true;
+	titlePlaceholder?: string;
+	titleValue?: string;
+};
+
+export type EditableCardProps = Omit<
+	React.ComponentProps<typeof Card>,
+	| 'isImageDisabled'
+	| 'imageUrl'
+	| 'content'
+	| 'title'
+	| 'onClick'
+	| 'isDisabled'
+> &
+	(EditableCardPropsWithTitle | EditableCardPropsWithoutTitle);
+
+const EditableCard: React.FC<EditableCardProps> = (props) => {
+	const {
+		bodyPlaceholder,
+		bodyValue,
+		onTitleChange,
+		onBodyChange,
+		...otherProps
+	} = props;
+
+	return (
+		<Card
+			{...otherProps}
+			isImageDisabled={true}
+			isDisabled={false}
+			className="editable-card"
+		>
+			{props.hasTitleField && (
+				<TextInput
+					variant="single"
+					value={props.titleValue}
+					placeholder={props.titlePlaceholder}
+					onChange={props.onTitleChange}
+				/>
+			)}
+			<TextInput
+				variant="multi"
+				value={bodyValue}
+				placeholder={bodyPlaceholder}
+				onChange={onBodyChange}
+				rows={14}
+			/>
+		</Card>
+	);
+};
+
+export default EditableCard;
--- src/components/EditableCard/__stories__/EditableCard.stories.tsx
+++ src/components/EditableCard/__stories__/EditableCard.stories.tsx
@@ -4,3 +4,12 @@ import EditableCard from '..';
 export default {
 	title: 'Components/EditableCard',
 } as ComponentMeta<typeof EditableCard>;
+
+const Template: ComponentStory<typeof EditableCard> = (args) => (
+	<EditableCard {...args} style={{ width: '200px' }} />
+);
+
+export const Default = Template.bind({});
+
+export const WithTitle = Template.bind({});
+WithTitle.args = { hasTitleField: true };
