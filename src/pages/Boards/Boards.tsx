bad326b91f18fb84476801fab1b42ecac3478f5e
4rgc <bohdan.andrii@gmail.com>
2022-08-09 09:40:42 -0400

    feat: add component logic for adding a board

--- src/pages/Boards/Boards.tsx
+++ src/pages/Boards/Boards.tsx
@@ -1,5 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Button from '../../components/Button';
 import Card from '../../components/Card';
+import EditableCard from '../../components/EditableCard';
+import GhostCard from '../../components/GhostCard';
 import useTrellzoAPI from '../../hooks/useTrellzoAPI';
 import PartialBoard from '../../types/PartialBoard';
 import APIRequestParams from '../../util/APIParams';
@@ -11,12 +14,20 @@ requestParams.setRoute('/board');
 const Boards = () => {
 	const [response, error, reload] =
 		useTrellzoAPI<{ boards: PartialBoard[] | undefined }>(requestParams);
+	const [isBoardBeingAdded, setIsBoardBeingAdded] = useState(false);
+	const [newBoardName, setNewBoardName] = useState('');
+	const [newBoardDescription, setNewBoardDescription] = useState('');
 
 	useEffect(() => {
 		reload();
 	}, [reload]);
 
 	const boards = response && response.boards;
+	const onBoardCreationCancel = () => {
+		setIsBoardBeingAdded(false);
+		setNewBoardName('');
+		setNewBoardDescription('');
+	};
 
 	return (
 		<div className="boards">
@@ -30,6 +41,38 @@ const Boards = () => {
 							style={{ margin: '5px' }}
 						/>
 					))}
+					{isBoardBeingAdded ? (
+						<EditableCard
+							size="md"
+							hasTitleField
+							titleValue={newBoardName}
+							onTitleChange={setNewBoardName}
+							titlePlaceholder="Name"
+							bodyValue={newBoardDescription}
+							bodyPlaceholder="Description"
+							onBodyChange={setNewBoardDescription}
+							hasButtons
+						>
+							<Button
+								height="thin"
+								size="small"
+								kind="secondary"
+								onClick={onBoardCreationCancel}
+							>
+								cancel
+							</Button>
+							<Button height="thin" size="small">
+								create
+							</Button>
+						</EditableCard>
+					) : (
+						<GhostCard
+							size="md"
+							isImageDisabled
+							innerText="Add a new board..."
+							onClick={() => setIsBoardBeingAdded(true)}
+						/>
+					)}
 				</div>
 			)}
 
