d7518db76a8024ba096452a990fab6631eec90ec
4rgc <bohdan.andrii@gmail.com>
2022-08-13 10:25:42 -0400

    refactor: move boards to state

--- src/pages/Boards/Boards.tsx
+++ src/pages/Boards/Boards.tsx
@@ -15,22 +15,21 @@ getBoardParams.setRoute('/board');
 const Boards = () => {
 	const [response, error, reload] =
 		useTrellzoAPI<{ boards: PartialBoard[] | undefined }>(getBoardParams);
-	const [boards_, setBoards_] = useState<PartialBoard[]>([]);
+	const [boards, setBoards] = useState<PartialBoard[]>([]);
 	const [isBoardBeingAdded, setIsBoardBeingAdded] = useState<boolean>(false);
 	const [newBoardName, setNewBoardName] = useState<string>('');
-	const [, , reloadCreate, changeCreateParams] = useTrellzoAPI<{
+	const [, createError, createBoard, changeCreateParams] = useTrellzoAPI<{
 		boards: PartialBoard | undefined;
-	}>(new APIRequestParams('get'));
+	}>(new APIRequestParams('post'));
 
 	useEffect(() => {
 		reload();
 	}, [reload]);
 
 	useEffect(() => {
-		setBoards_((response && response.boards) || []);
+		setBoards((response && response.boards) || []);
 	}, [response]);
 
-	const boards = response && response.boards;
 	const onBoardCreationCancel = () => {
 		setIsBoardBeingAdded(false);
 		setNewBoardName('');
@@ -42,55 +41,57 @@ const Boards = () => {
 		params.setBodyParam('name', newBoardName);
 
 		changeCreateParams(params);
-		reloadCreate().then(() => reload());
+		createBoard().then(() => {
+			setIsBoardBeingAdded(false);
+			setNewBoardName('');
+			reload();
+		});
 	};
 
 	return (
 		<div className="boards">
-			{boards && (
-				<div className="boards-container">
-					{boards.map((b) => (
-						<Card
-							key={b._id}
-							title={b.name}
-							content={b.description}
-							style={{ margin: '5px' }}
-						/>
-					))}
-					{isBoardBeingAdded ? (
-						<EditableCard
-							size="md"
-							bodyValue={newBoardName}
-							bodyPlaceholder="Name"
-							onBodyChange={setNewBoardName}
-							hasButtons
+			<div className="boards-container">
+				{boards.map((b) => (
+					<Card
+						key={b._id}
+						title={b.name}
+						content={b.description}
+						style={{ margin: '5px' }}
+					/>
+				))}
+				{isBoardBeingAdded ? (
+					<EditableCard
+						size="md"
+						bodyValue={newBoardName}
+						bodyPlaceholder="Name"
+						onBodyChange={setNewBoardName}
+						hasButtons
+					>
+						<Button
+							height="thin"
+							size="small"
+							kind="secondary"
+							onClick={onBoardCreationCancel}
 						>
-							<Button
-								height="thin"
-								size="small"
-								kind="secondary"
-								onClick={onBoardCreationCancel}
-							>
-								cancel
-							</Button>
-							<Button
-								height="thin"
-								size="small"
-								onClick={onBoardCreate}
-							>
-								create
-							</Button>
-						</EditableCard>
-					) : (
-						<GhostCard
-							size="md"
-							isImageDisabled
-							innerText="Add a new board..."
-							onClick={() => setIsBoardBeingAdded(true)}
-						/>
-					)}
-				</div>
-			)}
+							cancel
+						</Button>
+						<Button
+							height="thin"
+							size="small"
+							onClick={onBoardCreate}
+						>
+							create
+						</Button>
+					</EditableCard>
+				) : (
+					<GhostCard
+						size="md"
+						isImageDisabled
+						innerText="Add a new board..."
+						onClick={() => setIsBoardBeingAdded(true)}
+					/>
+				)}
+			</div>
 
 			<WarningFab displayOnMessage message={error?.message} />
 		</div>
