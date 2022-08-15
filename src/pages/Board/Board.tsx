2dad2266b87f4ba3501b33d44d9350df120358a3
4rgc <bohdan.andrii@gmail.com>
2022-08-15 11:14:47 -0400

    feat(Board): fetch board data and print it as JSON

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,7 +1,31 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import WarningFab from '../../components/WarningFab';
+import useTrellzoAPI from '../../hooks/useTrellzoAPI';
+import APIRequestParams from '../../util/APIParams';
 
 const Board: FC = () => {
-	return <></>;
+	const { id } = useParams();
+
+	const [boardData, boardError, reload, setParams] = useTrellzoAPI(
+		new APIRequestParams('get')
+	);
+
+	//FIXME: Infinitely loops with 401
+	useEffect(() => {
+		const apiParams = new APIRequestParams('get');
+		apiParams.setRoute(`/board/${id}`);
+
+		setParams(apiParams);
+		reload();
+	}, [id, setParams, reload]);
+
+	return (
+		<div className="board">
+			<code>{JSON.stringify(boardData && boardData.board, null, 2)}</code>
+			<WarningFab displayOnMessage message={boardError?.message} />
+		</div>
+	);
 };
 
 export default Board;
