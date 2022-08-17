17957923fd2da3eda0e1ee4f56a0eddbb2d4cb08
4rgc <bohdan.andrii@gmail.com>
2022-08-17 11:55:17 -0400

    feat(Board): employ useReducer to manage the board data

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,15 +1,51 @@
-import { FC, useEffect } from 'react';
+import { FC, Reducer, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import WarningFab from '../../components/WarningFab';
 import useTrellzoAPI from '../../hooks/useTrellzoAPI';
+import List from '../../types/List';
 import APIRequestParams from '../../util/APIParams';
+import './Board.scss';
+import BoardType from '../../types/Board';
+
+const initialLists: List[] = [];
+
+enum ActionTypes {
+	LIST_UPDATE,
+	LIST_CREATE,
+	LIST_DELETE,
+}
+
+type Action =
+	| { type: ActionTypes.LIST_UPDATE; data: List[] }
+	| { type: ActionTypes.LIST_CREATE; data: List }
+	| { type: ActionTypes.LIST_DELETE; data: string };
+
+const listsReducer: Reducer<List[], Action> = (state: List[], action) => {
+	switch (action.type) {
+		case ActionTypes.LIST_UPDATE:
+			//FIXME: sort the lists according to the order from the server
+			return [
+				...state.filter(
+					(stateBoard) =>
+						!action.data.find(
+							(updatedBoard) =>
+								updatedBoard._id === stateBoard._id
+						)
+				),
+				...action.data,
+			];
+		default:
+			throw new Error();
+	}
+};
 
 const Board: FC = () => {
 	const { id } = useParams();
 
-	const [boardData, boardError, reload, setParams] = useTrellzoAPI(
-		new APIRequestParams('get')
-	);
+	const [lists, dispatch] = useReducer(listsReducer, initialLists);
+	const [boardData, boardError, reload, setParams] = useTrellzoAPI<{
+		board: BoardType;
+	}>(new APIRequestParams('get'));
 
 	useEffect(() => {
 		const apiParams = new APIRequestParams('get');
@@ -21,7 +57,7 @@ const Board: FC = () => {
 
 	return (
 		<div className="board">
-			<code>{JSON.stringify(boardData && boardData.board, null, 2)}</code>
+			{lists && lists.map}
 			<WarningFab displayOnMessage message={boardError?.message} />
 		</div>
 	);
