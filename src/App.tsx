773d533a36c9c7442df83bd7bd161ea09e924f99
4rgc <bohdan.andrii@gmail.com>
2023-01-28 13:19:27 -0500

    feat: set up react-query to use in app

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,32 @@ import Board from './pages/Board';
 
 import Boards from './pages/Boards';
 import Login from './pages/Login';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
 	const location = useLocation();
 
 	return (
-		<div className="App">
-			{location.pathname !== '/login' && (
-				<>
-					<nav>
-						<Logo />
-					</nav>
-					<div style={{ border: '0.5px solid black' }} />
-				</>
-			)}
-			<Routes>
-				<Route path="/" element={<Navigate to="/login" />} />
-				<Route element={<Login />} path="login" />
-				<Route element={<Boards />} path="boards" />
-				<Route element={<Board />} path="b/:id" />
-			</Routes>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className="App">
+				{location.pathname !== '/login' && (
+					<>
+						<nav>
+							<Logo />
+						</nav>
+						<div style={{ border: '0.5px solid black' }} />
+					</>
+				)}
+				<Routes>
+					<Route path="/" element={<Navigate to="/login" />} />
+					<Route element={<Login />} path="login" />
+					<Route element={<Boards />} path="boards" />
+					<Route element={<Board />} path="b/:id" />
+				</Routes>
+			</div>
+		</QueryClientProvider>
 	);
 };
 
