8610630be7df70471ea92da93928012f4e71c9a2
4rgc <bohdan.andrii@gmail.com>
2023-03-04 14:32:00 -0500

    fix(LoadingBar): pass custom classes to the LoadingBar component

--- src/components/LoadingBar/LoadingBar.tsx
+++ src/components/LoadingBar/LoadingBar.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import './LoadingBar.scss';
 
-export interface LoadingBarProps {
+export interface LoadingBarProps extends React.ComponentPropsWithoutRef<'div'> {
 	state?: number;
 	size?: 'small' | 'large';
 	color?: string;
@@ -12,9 +12,10 @@ const LoadingBar: FC<LoadingBarProps> = ({
 	state = 0,
 	size = 'small',
 	color = 'green',
+	className,
 }) => {
 	return (
-		<div className={`loadbar-container loadbar-${size}`}>
+		<div className={`loadbar-container loadbar-${size} ${className}`}>
 			<div
 				className={`loadbar-inner`}
 				style={{ width: `${state}%`, backgroundColor: `${color}` }}
